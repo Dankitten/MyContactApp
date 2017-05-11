39a0235233730c8d6da21e2f91fe7dfe7463c9a2
Dankitten <veryfabulousbandit@gmail.com>
2017-05-11 09:20:56 -0700

    added DataBaseHelper

---
+++ MyContactApp/app/src/main/java/DataBaseHelper.java
@@ -0,0 +1,11 @@
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by wangk6771 on 5/11/2017.
+ */
+public class DataBaseHelper extends SQLiteOpenHelper{
+    
+    public DataBaseHelper(){
+        
+    }
+}
