261cb53b5c3badb0d2a7ee9269a318793a284a9b
Dankitten <veryfabulousbandit@gmail.com>
2017-05-15 09:27:57 -0700

    Changed some stuff and now there's errors everywhere

--- MyContactApp/app/src/main/java/com/example/wangk6771/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/example/wangk6771/mycontactapp/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.wangk6771.mycontactapp;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -21,5 +25,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myDb = new DataBaseHelper(this);
+
+       // editName = (EditText) findViewById(R.id.editText_name);
+        //editAge = (EditText) findViewById(R.id.editText_age);
+        //editAddress = (EditText) findViewById(R.id.editText_address);
     }
+
+    public void addData(View v){
+        boolean isInserted = myDb.insertData(editName.getText().toString());
+
+        if(isInserted == true){
+            Log.d("MyContact", "Data insertion successful");
+            //Create toast message to user indicating data inserted correctly
+
+        }
+        else{
+            Log.d("MyContact", "Data insertion not successful");
+            //Create toast message to user indicating data inserted incorrectly
+        }
+    }
+
+
 }
